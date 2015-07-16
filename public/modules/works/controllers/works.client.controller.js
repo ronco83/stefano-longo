42dfc9d07cfe2c485390f94f847477059be7f2d5
Stefano Longo <stefano.longo@mashfrog.com>
2015-07-16 18:04:26 +0200

    varie

--- public/modules/works/controllers/works.client.controller.js
+++ public/modules/works/controllers/works.client.controller.js
@@ -136,7 +136,7 @@ angular.module('works').controller('WorksController', ['$scope', '$stateParams',
 					"mouseleave": function(){$('.carousel-caption').css('opacity' , 0); $('.sliders img').css('opacity' , 1);}
 				});
 
-			},2000);
+			},3000);
 
 		}
 	}
